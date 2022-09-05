7aee7e60b82ee16a4b10bb4c48c26c6739a46122
mbright90 <michael.bright90@icloud.com>
2022-09-05 17:08:21 +0100

    import fontawesome functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import '@fortawesome/fontawesome-free/js/solid'
 import "./style/style.css"
+
+// Menu icon! innerHTML = "<i class='fa-solid fa-bars'></i>"
+// New project icon = "<i class='fa-solid fa-plus'></i>"
