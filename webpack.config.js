040faa38027484a926bbb8d1d4c9777fb67c7f0c
mbright90 <michael.bright90@icloud.com>
2022-09-05 17:23:32 +0100

    Comment out currently unnecessary code

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const path = require("path");
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "The You Do ToDo Space"
-        })
-    ],
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         title: "The You Do ToDo Space"
+    //     })
+    // ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
