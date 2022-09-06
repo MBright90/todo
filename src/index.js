96cbdabb255a51f90c7e172650722fbded446836
mbright90 <michael.bright90@icloud.com>
2022-09-06 17:18:33 +0100

    Comment in icon sources to use later

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import "./style/style.css"
 
 // Menu icon! innerHTML = "<i class='fa-solid fa-bars'></i>"
 // New project icon = "<i class='fa-solid fa-plus'></i>"
+
+
+/* ******* 
+    Edit icons
+
+    <a href=""><i class="fa-solid fa-check"></i></a>
+    <a href=""><i class="fa-regular fa-star"></i></a>
+    <a href=""><i class="fa-regular fa-pen-to-square"></i></a>
+    <a href=""><i class="fa-solid fa-trash"></i></a>
+    
+******* */
