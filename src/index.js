f4a3b83b8c233f76a0c26b450659f75e13e24e41
mbright90 <michael.bright90@icloud.com>
2022-09-07 10:22:56 +0100

    Comment example of empty message html elements

--- src/index.js
+++ src/index.js
@@ -12,5 +12,15 @@ import "./style/style.css"
     <a href=""><i class="fa-regular fa-star"></i></a>
     <a href=""><i class="fa-regular fa-pen-to-square"></i></a>
     <a href=""><i class="fa-solid fa-trash"></i></a>
-    
+
+    Empty home containers example
+
+    <div class="empty-container">
+        <div class="empty-deadlines-container">
+            <h1>Great News</h1>
+            <p>You have no upcoming deadlines</p>
+            <p>Create a new project from the sidebar.</p>
+        </div>
+    </div>
+
 ******* */
