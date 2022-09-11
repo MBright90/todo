9e918352e76c0935e43b2e5d9ef8dcc25fa4b145
mbright90 <michael.bright90@icloud.com>
2022-09-11 10:01:43 +0100

    Return functions for homeInterface

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -3,13 +3,25 @@ import { domManipulator as dom } from "./domManipulator";
 
 const homeInterface = (() => {
 
-    const createHomePage = () => {
+    const createHomepage = () => {
 
         // Code black to retrieve data //
 
         dom.initDashboard();
         dom.initHomepage();
     };
+
+    const updateDashboard = () => {
+        dom.removeCurrentMain();
+        dom.initHomepage();
+    }
+
+    // Functions to return
+
+    return {
+        createHomepage,
+        updateDashboard
+    }
 
 })();
 
