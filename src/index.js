a7dba7bfba1c3a4f14cca9ca93b1016ade088ad2
mbright90 <michael.bright90@icloud.com>
2022-09-11 10:02:54 +0100

    Remove comments which are no longer necessary

--- src/index.js
+++ src/index.js
@@ -7,21 +7,7 @@ import "./style/style.css"
 
 
 /* ******* 
-    Edit icons
 
-    <a href=""><i class="fa-solid fa-check"></i></a>
-    <a href=""><i class="fa-regular fa-star"></i></a>
-    <a href=""><i class="fa-regular fa-pen-to-square"></i></a>
-    <a href=""><i class="fa-solid fa-trash"></i></a>
 
-    Empty home containers example
-
-    <div class="empty-container">
-        <div class="empty-deadlines-container">
-            <h1>Great News</h1>
-            <p>You have no upcoming deadlines</p>
-            <p>Create a new project from the sidebar.</p>
-        </div>
-    </div>
 
 ******* */
