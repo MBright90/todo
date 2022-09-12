195b3d68d03fc6785c3fe34db49b536d9e40535c
mbright90 <michael.bright90@icloud.com>
2022-09-12 20:03:54 +0100

    Reorder imports to allow for functioning styles during build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-// import { pageInterface as page } from './utilities/utilities.js'
 import './assets/fontawesome-css/all.css'
 import "./style/style.css"
+
+import { pageInterface as page } from './utilities/utilities.js'
 
 // Menu icon! innerHTML = "<i class='fa-solid fa-bars'></i>"
 // New project icon = "<i class='fa-solid fa-plus'></i>"
