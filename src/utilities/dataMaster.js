1544f44be60f4660f29919c14de5af1d48131118
mbright90 <michael.bright90@icloud.com>
2022-09-12 16:46:22 +0100

    Add complete attribute for project todos

--- src/utilities/dataMaster.js
+++ src/utilities/dataMaster.js
@@ -1,23 +1,25 @@
 const dataMaster = (() => {
-    const _todoDataset = {};
+    const _todoDataset = {
+        general : [],
+        projects: [],
+    };
 
     // Local storage functions //
 
     const _retrieveLocalData = () => {
         const localData = {
-
             general: [
                 {
                     title: "toDo title",
                     description: "toDo description",
-                    dueDate: "25/09/2022",
+                    dueDate: new Date(2022, 09, 25),
                     important: true,
                     toDoID: 21862122
                 },
                 {
                     title: "toDo title two",
                     description: "toDo description two",
-                    dueDate: "27/09/2022",
+                    dueDate: new Date(2022, 09, 27),
                     important: false,
                     toDoID: 24862122
                 }
@@ -31,16 +33,18 @@ const dataMaster = (() => {
                         {
                             title: "project toDo one",
                             description: "project description one",
-                            dueDate: "30/09/2022",
+                            dueDate: new Date(30/09/2022, 9, 11),
                             important: true,
-                            toDoID: 23862122
+                            toDoID: 23862122,
+                            complete: False,
                         },
                         {
                             title: "project toDo two",
                             description: "project description two",
-                            dueDate: "03/10/2022",
+                            dueDate: new Date(2022, 10, 3),
                             important: true,
-                            toDoID: 22862122
+                            toDoID: 22862122,
+                            complete: false,
                         },
                     ]
                 },
@@ -52,14 +56,18 @@ const dataMaster = (() => {
                         {
                             title: "project toDo one",
                             description: "project description one",
-                            dueDate: "30/09/2022",
+                            dueDate: new Date(2022, 9, 30),
                             important: true,
+                            toDoID: 21827281,
+                            complete: false,
                         },
                         {
                             title: "project toDo two",
                             description: "project description two",
-                            dueDate: "03/10/2022",
+                            dueDate: new Date(2022, 10, 22),
                             important: true,
+                            toDoID: 2167125,
+                            complete: true,
                         },
                     ]
                 },
@@ -89,7 +97,7 @@ const dataMaster = (() => {
         const todoObject = {
             title: todoTitle,
             description: todoDescription,
-            dueDate: todoDueDate,
+            dueDate: new Date(todoDueDate),
             important: isImportant,
             toDoID: _createTodoID(),
         };
@@ -102,6 +110,7 @@ const dataMaster = (() => {
             projectImage: newProjectImage || null,
             projectID: _createProjectID(),
         };
+        return projectObject;
     };
 
     // Data deletion functions
@@ -121,7 +130,7 @@ const dataMaster = (() => {
     };
 
     const _deleteProject = (projectToDelete) => {
-        
+
     };
 
     // Dataset appending functions
@@ -131,7 +140,47 @@ const dataMaster = (() => {
     };
 
     const _appendProjectTodo = (todoObject, findProjectID) => {
+        todoObject.complete = False;
         _todoDataset.projects.find(proj => proj.projectID === findProjectID).projectToDos.push(todoObject);
     };
 
+    // Data retrieving functions
+
+    const _retrieveProject = (projectID) => {
+        const foundProject = _todoDataset.projects.find(proj => proj.projectID === projectID);
+        if (!foundProject) {
+            throw new Error `No Project Found with ID ${projectID}`
+        } else {
+            return foundProject;
+        };
+    };
+
+    const _retrieveAllTodos = () => {
+        const allToDos = _todoDataset.general;
+        _todoDataset.projects.forEach(project => {
+            project.projectToDos.forEach(toDo => {
+                allToDos.push(toDo);
+            });
+        });
+        return allToDos;
+    };
+
+    const _retrieveDateOrdered = () => {
+        const allToDos = _retrieveAllTodos();
+    };
+
+    // Functions to return
+
+    const addNewTodo = (title, description, dueDate) => {
+        const newTodo = _createTodoObject(title, description, dueDate);
+        _appendGeneralTodo(newTodo);
+    };
+
+    return {
+        addNewTodo,
+
+    };
+
 })();
+
+export { dataMaster };
