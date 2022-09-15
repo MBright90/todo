edc4eeb9a9945bba453a5952885a45d653dc6615
mbright90 <michael.bright90@icloud.com>
2022-09-15 09:58:02 +0100

    Initiate formMaster object

--- src/utilities/formMaster.js
+++ src/utilities/formMaster.js
@@ -1,5 +1,9 @@
 const formMaster = (() => {
 
+    const addTodoForm = () => {
+        const formElement = _createElementClass("form", "todoForm");
+    };
+
     return {
         newTodoForm,
         newProjectForm
