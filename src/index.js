a25670adc571f1a24b8b7dcbb3153b90bee5bf88
mbright90 <michael.bright90@icloud.com>
2022-09-17 11:49:15 +0100

    Create events object

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import "./style/style.css"
 
 import { pageInterface as page } from './utilities/utilities.js'
 
+const events = (() => {
+
+    const newListener = (element, action) => {}
+
+    return {
+        addHeaderListeners,
+        addSidebarListeners,
+        addHomepageListeners,
+        addProjectsListeners,
+    };
+
+})();
+
 page.createHomepage();
 
 /* ******* 
