7c0d8ca385dacfe8c07e5d1f6c593b46e1cf8f46
mbright90 <michael.bright90@icloud.com>
2022-09-17 14:13:54 +0100

    Remove event listeners

--- src/index.js
+++ src/index.js
@@ -3,19 +3,6 @@ import "./style/style.css"
 
 import { pageInterface as page } from './utilities/utilities.js'
 
-const events = (() => {
-
-    const newListener = (element, action) => {}
-
-    return {
-        addHeaderListeners,
-        addSidebarListeners,
-        addHomepageListeners,
-        addProjectsListeners,
-    };
-
-})();
-
 page.createHomepage();
 
 /* ******* 
