7e525ea156aefd6424e929163f9135acbb1a03dd
mbright90 <michael.bright90@icloud.com>
2022-09-17 11:49:48 +0100

    Remove code imported to index.js

--- src/utilities/eventMaster.js
+++ src/utilities/eventMaster.js
@@ -1,14 +1,3 @@
-const eventMaster = (() => {
 
-    const newListener = (element, action) => {}
-
-    return {
-        addHeaderListeners,
-        addSidebarListeners,
-        addHomepageListeners,
-        addProjectsListeners,
-    };
-
-})();
 
 export { eventMaster };
