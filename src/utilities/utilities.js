3db683497cc340678496c3f51a89babe79db143c
mbright90 <michael.bright90@icloud.com>
2022-09-17 14:13:38 +0100

    Add event listeners to page interface object

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -3,6 +3,57 @@ import { dataMaster as data } from "./dataMaster.js";
 
 const pageInterface = (() => {
 
+    // ********************************************* //
+    // ************** Event Listeners ************** //
+    // ********************************************* //
+
+    const _createDropLink = (aTag) => {
+        aTag.addEventListener("click", () => {
+            console.log("drop link CLICKED!")
+            // Code block to show menu
+        });
+    };
+
+    const _createAddLink = (aTag) => {
+        aTag.addEventListener("click", () => {
+            console.log("New link CLICKED!")
+            // Code block to show add form
+        });
+    };
+
+
+    const _createTimeLinks = (linkList, ...args) => {
+        for (let i = 0; i < linkList.length; i++) {
+            linkList[i].addEventListener("click", () => {
+                const requestedData = data.retrieveData(args[i]);
+                console.log(requestedData);
+                // Code block to display requested data
+            });
+        };
+    };
+
+    const _addHeaderListeners = () => {
+        _createDropLink(document.querySelector(".drop-link"));
+        _createAddLink(document.querySelector(".new-icon"));
+    };
+
+    const _addSidebarListeners = () => {
+        _createTimeLinks(document.querySelectorAll(".date-links li a"), "date", 1, 7, 31);
+    }
+
+    // const newListener = (element, action) => {}
+
+    // return {
+    //     addHeaderListeners,
+    //     addSidebarListeners,
+    //     addHomepageListeners,
+    //     addProjectsListeners,
+    // };
+
+    // ********************************************* //
+    // ************** Dom creation IF ************** //
+    // ********************************************* //
+
     const createHomepage = () => {
         dom.initDashboard();
         dom.initHomepage(
@@ -19,6 +70,7 @@ const pageInterface = (() => {
 
     // const showAllTodos = () => {}
     // const showProjects = () => {}
+
 
     // Functions to return
 
