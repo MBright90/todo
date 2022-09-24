193ffe8fbadf6919fdc4d891fc74c46d17388ff8
mbright90 <michael.bright90@icloud.com>
2022-09-24 10:14:06 +0100

    Refactor form creation

--- src/utilities/domManipulator.js
+++ src/utilities/domManipulator.js
@@ -588,6 +588,76 @@ const formMaster = (() => {
         return createElementClass("i", "fa-solid", "fa-xmark" , "close-form");
     };
 
+    const _createTitleInput = (titleId, currentTitle) => {
+        currentTitle = currentTitle || null;
+
+        const titleLabel = createElementText("label", "Title");
+        titleLabel.setAttribute("for", titleId);
+        const titleInput = document.createElement("input");
+        setAttributes(titleInput, {
+            "type": "text",
+            "name": titleId,
+            "id": titleId,
+            "min": 3,
+            "max": 30,
+            "required": "",
+        });
+        if (currentTitle) titleInput.textContent = currentTitle;
+        return [titleLabel, titleInput];
+    };
+
+    const _createDescriptionInput = (descriptionId, currentDescription) => {
+        currentDescription = currentDescription || null;
+
+        const descriptionLabel = createElementText("label", "Description");
+        descriptionLabel.setAttribute("for", descriptionId);
+        const descriptionInput = document.createElement("textarea");
+        setAttributes(descriptionInput, {
+            "name": descriptionId,
+            "id": descriptionId,
+            "cols": 30,
+            "rows": 10,
+            "max": 200,
+            "required": "",
+        });
+        if (currentDescription) descriptionInput.textContent = currentDescription;
+        return [descriptionLabel, descriptionInput];
+    };
+
+    const _createDueInput = (dueDateId, currentDueDate) => {
+        currentDueDate = currentDueDate || null;
+
+        const dueDateLabel = createElementText("label", "Due Date");
+        dueDateLabel.setAttribute("for", dueDateId);
+        const dueDateInput = document.createElement("input");
+        setAttributes(dueDateInput, {
+            "type": "date",
+            "name": dueDateId,
+            "id": dueDateId,
+            "min": _minDateInput(),
+            "max": _maxDateInput(),
+            "required": "",
+        });
+        if (currentDueDate) dueDateInput.value = currentDueDate;
+        return [dueDateLabel, dueDateInput];
+    };
+
+    const _createImageInput = (imageInputId, currentImageUrl) => {
+        currentImageUrl = currentImageUrl || null;
+
+        const imageLabel = createElementText("label", "Image URL");
+        imageLabel.setAttribute("for", imageInputId);
+        const imageInput = document.createElement("input");
+        setAttributes(imageInput, {
+            "type": "url",
+            "name": imageInputId,
+            "id": imageInputId,
+            "placeholder": "[Optional]"
+        });
+        if (currentImageUrl) imageInput.textContent = currentImageUrl;
+        return [imageLabel, imageInput];
+    };
+
     const createTodoForm = (projectToLink) => {
         projectToLink = projectToLink || null;
         
@@ -597,50 +667,18 @@ const formMaster = (() => {
 
         const fieldsetLegend = createElementText("legend", "New ToDo");
 
-        const titleLabel = createElementText("label", "Title");
-        titleLabel.setAttribute("for", "title-input");
-        const titleInput = document.createElement("input");
-        setAttributes(titleInput, {
-            "type": "text",
-            "name": "title-input",
-            "id": "title-input",
-            "min": 3,
-            "max": 30,
-            "required": "",
-        });
-
-        const descriptionLabel = createElementText("label", "Description");
-        descriptionLabel.setAttribute("for", "description-input");
-        const descriptionInput = document.createElement("textarea");
-        setAttributes(descriptionInput, {
-            "name": "descriptionInput",
-            "id": "description-input",
-            "cols": 30,
-            "rows": 10,
-            "max": 200,
-            "required": "",
-        });
-
-        const dueDateLabel = createElementText("label", "Due Date");
-        dueDateLabel.setAttribute("for", "due-date-input");
-        const dueDateInput = document.createElement("input");
-        setAttributes(dueDateInput, {
-            "type": "date",
-            "name": "due-date-input",
-            "id": "due-date-input",
-            "min": _minDateInput(),
-            "max": _maxDateInput(),
-            "required": "",
-        });
+        const titleArr = _createTitleInput("title-input");
+        const descriptionArr = _createDescriptionInput("description-input");
+        const dueDateArr = _createDueInput("due-date-input");
 
         appendChildren(fieldsetElement,
             fieldsetLegend,
-            titleLabel,
-            titleInput,
-            descriptionLabel,
-            descriptionInput,
-            dueDateLabel,
-            dueDateInput,
+            titleArr[0],
+            titleArr[1],
+            descriptionArr[0],
+            descriptionArr[1],
+            dueDateArr[0],
+            dueDateArr[1],
             _createSubmitButton(projectToLink),
         );
 
@@ -658,49 +696,80 @@ const formMaster = (() => {
 
         const fieldsetLegend = createElementText("legend", "New Project");
 
-        const titleLabel = createElementText("label", "Project Title");
-        titleLabel.setAttribute("for", "project-title-input");
-        const titleInput = document.createElement("input");
-        setAttributes(titleInput, {
-            "type": "text",
-            "name": "project-title-input",
-            "id": "project-title-input",
-            "min": 3,
-            "max": 30,
-            "required": "",
-        }); 
-
-        const descriptionLabel = createElementText("label", "Project Overview");
-        descriptionLabel.setAttribute("for", "project-description-input");
-        const descriptionInput = document.createElement("textarea");
-        setAttributes(descriptionInput, {
-            "name": "project-description-input",
-            "id": "project-description-input",
-            "cols": 30,
-            "rows": 10,
-            "min": 5,
-            "max": 200,
-            "required": "",
-        });
-
-        const imageLabel = createElementText("label", "Image URL");
-        imageLabel.setAttribute("for", "image-input");
-        const imageInput = document.createElement("input");
-        setAttributes(imageInput, {
-            "type": "url",
-            "name": "image-input",
-            "id": "image-input",
-            "placeholder": "[Optional]"
-        });
+        const titleArr = _createTitleInput("project-title-input");
+        const descriptionArr = _createDescriptionInput("project-description-input");
+        const imageArr = _createImageInput("image-input");
 
         appendChildren(fieldsetElement,
             fieldsetLegend,
-            titleLabel,
-            titleInput,
-            descriptionLabel,
-            descriptionInput,
-            imageLabel,
-            imageInput,
+            titleArr[0],
+            titleArr[1],
+            descriptionArr[0],
+            descriptionArr[1],
+            imageArr[0],
+            imageArr[1],
+            _createSubmitButton(),
+        );
+
+        formElement.appendChild(fieldsetElement);
+        appendChildren(formContainer,
+            formElement,
+            _createCloseButton());
+        return formContainer;
+    };
+
+    function editTodoForm(previousTitle, previousDescription, previousDueDate, projectToLink) {
+        projectToLink = projectToLink || null;
+        
+        const formContainer = createElementClass("div", "form-container");
+        const formElement = createElementClass("form", "todo-form");
+        const fieldsetElement = document.createElement("fieldset");
+
+        const fieldsetLegend = createElementText("legend", "Edit ToDo");
+
+        const titleArr = _createTitleInput("title-input", previousTitle);
+        const descriptionArr = _createDescriptionInput("description-input", previousDescription);
+        const dueDateArr = _createDueInput("due-date-input", previousDueDate);
+
+        appendChildren(fieldsetElement,
+            fieldsetLegend,
+            titleArr[0],
+            titleArr[1],
+            descriptionArr[0],
+            descriptionArr[1],
+            dueDateArr[0],
+            dueDateArr[1],
+            _createSubmitButton(projectToLink),
+        );
+
+        formElement.appendChild(fieldsetElement);
+        appendChildren(formContainer,
+            formElement,
+            _createCloseButton());
+        return formContainer;
+    };
+
+    function editProjectForm(previousTitle, previousDescription, previousImageURL) {
+        previousImageURL = previousImageURL || null;
+
+        const formContainer = createElementClass("div", "form-container")
+        const formElement = createElementClass("form", "project-form");
+        const fieldsetElement = document.createElement("fieldset");
+
+        const fieldsetLegend = createElementText("legend", "Edit Project");
+
+        const titleArr = _createTitleInput("project-title-input", previousTitle);
+        const descriptionArr = _createDescriptionInput("project-description-input", previousDescription);
+        const imageArr = _createImageInput("image-input", previousImageURL);  
+        
+        appendChildren(fieldsetElement,
+            fieldsetLegend,
+            titleArr[0],
+            titleArr[1],
+            descriptionArr[0],
+            descriptionArr[1],
+            imageArr[0],
+            imageArr[1],
             _createSubmitButton(),
         );
 
@@ -740,7 +809,9 @@ const formMaster = (() => {
 
     return {
         createTodoForm,
+        editTodoForm,
         createProjectForm,
+        editProjectForm,
         checkFormValidity
     };
 
