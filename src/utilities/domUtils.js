843a9a90de35835df77d70a16472a9a898a46a8c
mbright90 <michael.bright90@icloud.com>
2022-09-28 10:19:11 +0100

    Initiate domUtils.js Initiation refactoring

---
+++ src/utilities/domUtils.js
@@ -0,0 +1,39 @@
+const domUtils = (() => {
+    const createElementClass = (element, ...args) => {
+        const newElement = document.createElement(element);
+        args.forEach(arg => {
+            newElement.classList.add(arg);
+        });
+        return newElement;
+    };
+
+    function createElementText(element, text) {
+        const newElement = document.createElement(element);
+        try {
+            newElement.textContent = text;
+        }
+        catch(err) {
+            console.log("No text can be added to this element");
+        };
+        return newElement;
+    };
+
+    function appendChildren(element, ...args) {
+        args.forEach(arg => {
+            element.appendChild(arg);
+        });
+    };
+
+    function setAttributes(element, attributes) {
+        for (const key in attributes) {
+            element.setAttribute(key, attributes[key])
+        };
+    };
+
+    return createElementClass,
+        createElementText,
+        appendChildren,
+        setAttributes
+})();
+
+export { domUtils }
